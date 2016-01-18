bb5a6bed0d1871826c4bbbbb2b847729f37555a3
Willian Mitsuda <wmitsuda@gmail.com>
2016-01-18 07:14:25 -0200

    Solve the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Make a cacheable matrix from an existing matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setsolve <- function(i) {
+    s <<- i
+  }
+  getsolve <- function() {
+    s
+  }
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## Calculates the inverse of a cacheable matrix created originally from makeCacheMatrix()
+##
+## If the inverse was already calculated, return it from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("Getting cached result")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setsolve(s)
+  s
 }
